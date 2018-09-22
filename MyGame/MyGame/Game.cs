4bec696daa367df473537147b2a93d11ee8f9b43
Decktare <BANG34@yandex.ru>
2018-09-22 12:10:59 +0400

    Тестирование передвижения корабля

--- MyGame/MyGame/Game.cs
+++ MyGame/MyGame/Game.cs
@@ -37,7 +37,8 @@ namespace MyGame
             Timer timer = new Timer { Interval = 100 };
             timer.Start();
             timer.Tick += Timer_Tick;
-            
+            form.KeyDown += Form_KeyDown;
+
         }
         private static void Timer_Tick(object sender, EventArgs e)
         {
@@ -87,12 +88,19 @@ namespace MyGame
             _objs[37] = new Planet(new Point(random.Next(0, Width - 120), random.Next(0, Height - 120)), new Point(4, 4), new Size(0, 0));
             _objs[38] = new RazlomPlanet(new Point(random.Next(0, Width- 120), random.Next(0, Height- 120)), new Point(3, 3), new Size(0, 0));
             _objs[39] = new KolcaPlanet(new Point(random.Next(0, Width - 120), random.Next(0, Height - 120)), new Point(2, 2), new Size(0, 0));
-            _bullet = new Bullet(new Point(0, 200), new Point(5, 0), new Size(50, 45));
+            //_bullet = new Bullet(new Point(0, 200), new Point(5, 0), new Size(50, 45));
             for (var i = 0; i < _asteroids.Length; i++)
             {                
                 _asteroids[i] = new Asteroid(new Point(random.Next(0, 750), 200), new Point(10, 10), new Size(5, 45));
             }
-        }
 
+        }
+        private static Ship _ship = new Ship(new Point(10, 400), new Point(5, 5), new Size(10, 10));
+        private static void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey) _bullet = new Bullet(new Point(_ship.Rect.X + 10, _ship.Rect.Y + 4), new Point(4, 0), new Size(4, 1));
+            if (e.KeyCode == Keys.Up) _ship.Up();
+            if (e.KeyCode == Keys.Down) _ship.Down();
+        }
     }
 }
